34b7544ce0236b12f185ebac59fa1d57e3bb3ebb
jelly octane <jamie.de.lange@mac.com>
2021-01-24 16:54:53 -0600

    push

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,12 +92,15 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-   Post.upvote(req.body, { Vote })
-   .then(updatedPostData => res.json(updatedPostData))
-   .catch(err => {
-       console.log(err);
-       res.status(400).json(err);
-   });
+   if (req.session) {
+       // pass session id along with destructured properties on req.body
+       Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+       .then(updatedVoteData => res.json(updatedVoteData))
+       .catch(err => {
+           console.log(err);
+           res.status(500).json(err);
+       });
+   }
 });
 
 // update post title
