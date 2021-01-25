ae7b8dd43831c92f262a59a56aef76a02a45c633
jelly octane <jamie.de.lange@mac.com>
2021-01-24 18:18:36 -0600

    post page, commenting and upvoting complete

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/connection');
 // create Post model
 class Post extends Model {
     static upvote(body, models) {
+        console.log({body});
         return models.Vote.create({
             user_id: body.user_id,
             post_id: body.post_id
